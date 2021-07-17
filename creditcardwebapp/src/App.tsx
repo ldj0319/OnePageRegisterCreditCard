33481c64ac56db4eb57334b7f67830954a5942ca
unknown <ldj970319@gmail.com>
2021-07-17 23:38:15 +1200

    Add HomePage

--- creditcardwebapp/src/App.tsx
+++ creditcardwebapp/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Sidebar from './components/Sidebar';
 import './App.css';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
     <div className="App">
         <Sidebar />
+        <HomePage />
     </div>
   );
 }
