89fedb89b48cc33f6eadb7b3ac6cb8a45469d0dd
unknown <ldj970319@gmail.com>
2021-07-18 00:03:06 +1200

    Add submit button and validation

--- creditcardwebapp/src/components/CreditCardForm.tsx
+++ creditcardwebapp/src/components/CreditCardForm.tsx
@@ -6,10 +6,26 @@ const CreditCardForm = () => {
     const [creditCardNumber, setCreditCardNumber] = useState(0);
     const [CVC, setCVC] = useState(0);
     const [expiryDate, setExpiryDate] = useState(new Date());
-    
+    const [valid, setValid] = useState(false);
+
+    const Submit = () => {
+        if (valid) {
+            console.log("Credit Card Number: ", creditCardNumber);
+            console.log("CVC Number: ", CVC);
+            console.log("Expiry Date: ", expiryDate);
+        } else {
+            console.log("Failed");
+        }
+    }
+
+    const Validate = () => {
+        if (creditCardNumber > 0 && CVC > 0 && expiryDate != null) {
+            setValid(true);
+        }
+    }
+
     return (
         <div className="CreditCardForm">
-
             <div className="CreditCardNumberField">
                 <input 
                     className="CreditCardNumberInput"
@@ -17,7 +33,6 @@ const CreditCardForm = () => {
                     placeholder="Credit Card Number" 
                     type="number"/>
             </div>
-
             <div className="CVCField">
                 <input 
                     className="CVCInput"
@@ -25,12 +40,14 @@ const CreditCardForm = () => {
                     placeholder="CVC" 
                     type="number"/>
             </div>
-
             <div className="ExpiryDateField">
                 <DatePicker className="ExpiryDateInput" 
                     onChange={ setExpiryDate } 
                     value={ expiryDate } />
             </div>
+            <button onClick={ Submit }>
+                Submit
+            </button>
         </div>
     );
 };
